1d3f92bde99469775d9a74e2ed768b7614ed0d0e
vgshenshin <vg.shenshin@gmail.com>
2022-10-25 12:36:54 +0300

    used props and key props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import EmpoyeesAddForm from '../employees-add-form/empoyees-add-form';
 import './app.css';
 
 function App() {
+
+    // имитируем получение данных с сервера
+    const data = [
+        {name: "John A.", salary: 900, increase: false, id: 1},
+        {name: "Alex M.", salary: 400, increase: true, id: 2},
+        {name: "Carl C.", salary: 500, increase: false, id: 3},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,8 +23,8 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-
-            <EmployeesList/>
+            {/* передача данных с сервера в виде пропсов */}
+            <EmployeesList data={data}/>  
             <EmpoyeesAddForm/>
         </div>
     );
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,24 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+    //  исп-ем данные с сервера в пропсах - деструктурируем массив data с объектами
+    //  исп-я метод map рендерим на странице item'ы 
+    const elements = data.map(item => {
+//  достаем keys деструктурируя data, а остальные св-ва передаем в itemProps - спред оператором ...
+        const {id, ...itemProps} = item;
+
+        //  формируем массив с item'ами с уник.ключами (keys) и остальными пропсами в нем
+        return (
+            // <EmployeesListItem name={item.name} salary={item.salary}/>
+            <EmployeesListItem key={id} {...itemProps}/>
+        )
+    })
+
+    //  возвращаем массив с item'ами в ul
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     );
 }
